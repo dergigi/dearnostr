ada58bacb0c83d8d31e1cedfa6c8c308e487ab03
Gigi <dergigi@pm.me>
2025-11-05 00:47:54 +0100

    refactor: remove verbose 'No signer found.' from extension message

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,4 +10,4 @@ export const DEAR_NOSTR_HASHTAG = "DearNostr";
 export const ALBY_EXTENSION_URL = "https://getalby.com/alby-extension";
 export const NOS2X_EXTENSION_URL = "https://github.com/fiatjaf/nos2x";
 
-export const EXTENSION_INSTALL_MESSAGE = "No signer found. Install a nostr extension like Alby or nos2x to unlock the diary.";
+export const EXTENSION_INSTALL_MESSAGE = "Install a nostr extension like Alby or nos2x to unlock the diary.";
