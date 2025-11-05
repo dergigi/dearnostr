df7b8c1ea2718faccd08dd548189ba393087e01b
Gigi <dergigi@pm.me>
2025-11-05 01:01:04 +0100

    feat: replace button icon with Font Awesome fa-pen-fancy icon

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -7,6 +7,9 @@ import { useObservableMemo } from "applesauce-react/hooks";
 import { stripEmojis } from "@/lib/utils";
 import { DEAR_NOSTR_PREFIX, DEAR_NOSTR_HASHTAG } from "@/lib/constants";
 import { useState, useRef, useEffect, useMemo } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenFancy } from "@fortawesome/free-solid-svg-icons";
+import "@/lib/fontawesome";
 
 export default function PostForm({
   pubkey,
@@ -159,9 +162,7 @@ export default function PostForm({
           </>
         ) : (
           <>
-            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-            </svg>
+            <FontAwesomeIcon icon={faPenFancy} className="w-5 h-5" />
             <span>Sign & Send</span>
           </>
         )}
